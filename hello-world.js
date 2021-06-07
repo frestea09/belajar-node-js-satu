535c5e6e8a75b0466a4dcb9fc0f5576ed4cadc95
ilman prasetya <ilmanfrasetya@gmail.com>
2021-06-07 10:43:51 +0700

    install npm

---
+++ hello-world.js
@@ -0,0 +1,3 @@
+const helloWorld = require('./ngetest-function');
+let temp = helloWorld.tambah(1,2);
+console.log(temp);
---
+++ ngetest-function.js
@@ -0,0 +1,8 @@
+const helloWorld = () =>{
+    console.log('hello world');
+}
+function tambah(bilanganPertama,bilanganKedua){
+    return bilanganPertama + bilanganKedua;
+}
+
+module.exports = tambah;
